24d9b227e3537221c1f1a8f784574df921b1afa3
gforbis <gforbis@TMP-DEV2.rolledalloys.com>
2019-04-10 15:25:02 -0400

    Added basic performance tool gwf.utils.Perf

---
+++ java8-utils/src/java/main/gwf/utils/Perf.java
@@ -0,0 +1,69 @@
+/*
+ * Copyright (c) 2019 BTS-IT, Inc. All Rights Reserved.
+ * The source code for this program is not published or otherwise divested of 
+ * its trade secrets, irrespective of what has been deposited with the U.S. 
+ * Copyright Office.
+ */
+package gwf.utils;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.log4j.LogManager;
+import org.apache.log4j.Logger;
+
+public class Perf {
+    private static final Logger LOG = LogManager.getLogger(Perf.class);
+    private static final Perf instance = new Perf();
+    private final Map<String,Long> runTime = new HashMap<>();
+    private final Map<String,Integer> runCount = new HashMap<>();
+    private long minDuration = 1;
+
+    private Perf() {
+        
+    }
+    
+    public static void start() {
+        StackTraceElement e = Thread.currentThread().getStackTrace()[2];
+        String id = e.getClassName() + "->"+e.getMethodName();
+        start(id);
+    }
+    
+    public static void start(String id) {
+        Long current = instance.runTime.put(id, System.currentTimeMillis());
+        if (null != current) {
+            LOG.info("Monitor " + id + " was restarted without a stop event");
+        }
+    }
+    
+    public static void stop() {
+        StackTraceElement e = Thread.currentThread().getStackTrace()[2];
+        String id = e.getClassName() + "->"+e.getMethodName();
+        stop(id);
+    }
+    
+    public static void stop(String id) {
+        long stop = System.currentTimeMillis();
+        Long start = instance.runTime.get(id);
+        Integer count = instance.runCount.get(id);
+        count = null == count ? 0 : count;
+        instance.runCount.put(id, ++count);
+        if (null == start) {
+            LOG.info("Monitor " + id + " was stopped without a start event");
+        } else {
+            long duration = stop - start.longValue();
+            if (duration >= instance.minDuration) {
+                StringBuffer sb = new StringBuffer();
+                sb.append(id);
+                if (count > 1) {
+                    sb.append(" run ");
+                    sb.append(count);
+                }
+                sb.append(" completed in ");
+                sb.append(stop - start);
+                sb.append(" ms");
+                LOG.info(sb.toString());            
+            }
+        }
+    }
+}
